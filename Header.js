6f8d833414941ac3d67ad99576d2d4f75c2d7c18
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2024-05-12 05:20:03 +0500

    Update Header.js

--- Header.js
+++ Header.js
@@ -50,6 +50,16 @@ const Header = () => {
                             </ul>
                         </div>
                     </div>
+
+                    <button className="block sm:hidden transition absolute right-10 top-10" onClick={handleClick}>
+                        {click ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+<path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+}
+                    </button>
+        
                 </div>
             </nav>
         </>
